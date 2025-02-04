668a037e46372aa11317aad64d855696c74ad46f
ec <ecyanofsky@gmail.com>
2025-02-04 13:51:27 +0200

    clean tsfiles

--- products/project_ECS/us-east-1/my/infrastructure/1-securitygroup/.terragrunt-cache/odWtP6IpdWpqJjpQU-7PtqIOlCk/BkZISKOmVzkyr3iku4RNTyIE-9Q/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ecs_sg_id" {
-  description = "The ID of the ECS Security Group."
-  value       = aws_security_group.ecs_sg.id
-}
--- products/project_ECS/us-east-1/my/infrastructure/5-nlb/.terragrunt-cache/HlbZMK9p6AdEGae0Dmfj9QMNoUE/OT1bGuCtzh4lM6PiJWCsbSlfnu8/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "launch_template_id" {
-  description = "ID of the created ECS Launch Template"
-  value       = aws_launch_template.ecs_lt.id
-}
-
-output "launch_template_name" {
-  description = "Name of the created ECS Launch Template"
-  value       = aws_launch_template.ecs_lt.name
-}
